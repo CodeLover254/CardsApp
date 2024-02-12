4732ca26056e0510103ad2d9bbe297205e3e9fa7
Patrick <pmwangi@bridgeinnovation.io>
2024-02-12 21:52:58 +0300

    chore: Add ApiResult class

---
+++ CardsApp.Domain/Dto/ApiResult.cs
@@ -0,0 +1,8 @@
+﻿namespace CardsApp.Domain.Dto;
+
+public class ApiResult<T>
+{
+    public T Result { get; set; }
+    public string Message { get; set; }
+    public List<string> Errors { get; set; } = new ();
+}
