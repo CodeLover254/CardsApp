b1d01cdcfa4eba8e25818f071935b53925d7e0d4
Patrick <pmwangi@bridgeinnovation.io>
2024-02-12 22:46:37 +0300

    chore: Add validators

---
+++ CardsApp.Application/Validators/Auth/UserLoginCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using CardsApp.Application.Commands.Auth;
+using FluentValidation;
+
+namespace CardsApp.Application.Validators.Auth;
+
+public class UserLoginCommandValidator: AbstractValidator<UserLoginCommand>
+{
+    public UserLoginCommandValidator()
+    {
+        //todo complete validator
+    }
+}
---
+++ CardsApp.Application/Validators/Cards/CreateCardCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using CardsApp.Application.Commands.Cards;
+using FluentValidation;
+
+namespace CardsApp.Application.Validators.Cards;
+
+public class CreateCardCommandValidator: AbstractValidator<CreateCardCommand>
+{
+    public CreateCardCommandValidator()
+    {
+        //todo complete validation
+    }
+}
