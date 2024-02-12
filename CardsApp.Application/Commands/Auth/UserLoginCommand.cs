a8224460f3963e2d836f28b536b56e10a57fbca7
Patrick <pmwangi@bridgeinnovation.io>
2024-02-12 22:47:30 +0300

    chore: Bootstrap UserLoginCommand

--- CardsApp.Application/Commands/Auth/UserLoginCommand.cs
+++ CardsApp.Application/Commands/Auth/UserLoginCommand.cs
@@ -1,6 +1,38 @@
-﻿namespace CardsApp.Application.Commands.Auth;
+﻿using CardsApp.Domain.Dto;
+using CardsApp.Domain.Dto.Auth;
+using CardsApp.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
 
-public class UserLoginCommand
+namespace CardsApp.Application.Commands.Auth;
+
+public class UserLoginCommand: IRequest<ApiResult<UserLoginResponse?>>
 {
-    
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
+
+public class UserLoginCommandHandler : IRequestHandler<UserLoginCommand, ApiResult<UserLoginResponse?>>
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+    private readonly RoleManager<ApplicationUser> _roleManager;
+    private readonly ILogger<UserLoginCommandHandler> _logger;
+
+    public UserLoginCommandHandler(UserManager<ApplicationUser> userManager, 
+        SignInManager<ApplicationUser> signInManager, 
+        RoleManager<ApplicationUser> roleManager, 
+        ILogger<UserLoginCommandHandler> logger)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+        _roleManager = roleManager;
+        _logger = logger;
+    }
+
+    public Task<ApiResult<UserLoginResponse?>> Handle(UserLoginCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
