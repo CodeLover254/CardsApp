385a8dcd83ca6f0df121634a768ae64f940feb96
Patrick <pmwangi@bridgeinnovation.io>
2024-02-13 20:49:36 +0300

    chore: Move file to different namespace

--- CardsApp.Domain/Dto/ApiResult.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CardsApp.Domain.Dto;
-
-public class ApiResult<T>
-{
-    public T Result { get; set; }
-    public string Message { get; set; }
-    public List<string> Errors { get; set; } = new ();
-}
