9f4157c87423caef4fcafca2acf33b6823678463
Patrick <pmwangi@bridgeinnovation.io>
2024-02-13 20:46:07 +0300

    chore: Add logic to create card

--- CardsApp.Application/Commands/Cards/CreateCardCommand.cs
+++ CardsApp.Application/Commands/Cards/CreateCardCommand.cs
@@ -1,36 +1,58 @@
-﻿using System.Security.Claims;
+﻿using CardsApp.Application.Interfaces;
 using CardsApp.Domain;
 using CardsApp.Domain.Dto;
 using CardsApp.Domain.Dto.Cards;
+using CardsApp.Domain.Entities;
+using CardsApp.Domain.Enums;
+using CardsApp.Domain.Mappers.Cards;
 using MediatR;
 using Microsoft.Extensions.Logging;
 
 namespace CardsApp.Application.Commands.Cards;
 
-public class CreateCardCommand: IRequest<ApiResult<CreateCardResponse?>>
+public class CreateCardCommand: CardRequest, IRequest<ApiResult<CardResponse?>>
 {
-    public string Name { get; set; }
-    public string? Description { get; set; }
-    public string? Color { get; set; }
 }
 
-public class CreateCardCommandHandler : IRequestHandler<CreateCardCommand, ApiResult<CreateCardResponse?>>
+public class CreateCardCommandHandler : IRequestHandler<CreateCardCommand, ApiResult<CardResponse?>>
 {
-    private readonly ClaimsPrincipal _claimsPrincipal;
+    private readonly ICurrentUserProvider _currentUserProvider;
     private readonly CardAppDbContext _dbContext;
     private readonly ILogger<CreateCardCommandHandler> _logger;
+    private readonly CardEntityToResponseMapper _mapper;
 
-    public CreateCardCommandHandler(ClaimsPrincipal claimsPrincipal,
+    public CreateCardCommandHandler(
         CardAppDbContext dbContext, 
-        ILogger<CreateCardCommandHandler> logger)
+        ILogger<CreateCardCommandHandler> logger, 
+        ICurrentUserProvider currentUserProvider, 
+        CardEntityToResponseMapper mapper)
     {
-        _claimsPrincipal = claimsPrincipal;
         _dbContext = dbContext;
         _logger = logger;
+        _currentUserProvider = currentUserProvider;
+        _mapper = mapper;
     }
 
-    public Task<ApiResult<CreateCardResponse?>> Handle(CreateCardCommand request, CancellationToken cancellationToken)
+    public async Task<ApiResult<CardResponse?>> Handle(CreateCardCommand request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var card = new Card
+        {
+            Name = request.Name,
+            Description = request.Description,
+            Color = request.Color,
+            UserId = _currentUserProvider.UserId!
+        };
+
+        _dbContext.Cards.Add(card);
+        var affectedRows = await _dbContext.SaveChangesAsync(cancellationToken);
+        
+        if (affectedRows > 0)
+        {
+            return ResponseMessage<CardResponse?>.Success(_mapper.MapToResponse(card), 
+                "Card successfully created", 
+                ResponseCodes.Created);
+        }
+        
+        return ResponseMessage<CardResponse?>.Error(null, "Unable to create card");
     }
 }
