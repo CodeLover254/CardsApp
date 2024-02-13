67f30a942ae80e07b82791b9e3516537908b192b
Patrick <pmwangi@bridgeinnovation.io>
2024-02-13 20:53:17 +0300

    chore: Add logic to update cards

---
+++ CardsApp.Application/Commands/Cards/UpdateCardCommand.cs
@@ -0,0 +1,55 @@
+﻿using CardsApp.Application.Interfaces;
+using CardsApp.Domain;
+using CardsApp.Domain.Dto.Cards;
+using CardsApp.Domain.Dto.Results;
+using CardsApp.Domain.Enums;
+using CardsApp.Domain.Mappers.Cards;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace CardsApp.Application.Commands.Cards;
+
+public class UpdateCardCommand: CardRequest, IRequest<ApiResult<CardResponse?>>
+{
+    public string Id { get; set; }
+    public CardStatus Status { get; set; }
+}
+
+public class UpdateCardCommandHandler : IRequestHandler<UpdateCardCommand, ApiResult<CardResponse?>>
+{
+    private readonly ICurrentUserProvider _currentUserProvider;
+    private readonly CardAppDbContext _dbContext;
+    private readonly CardEntityToResponseMapper _mapper;
+
+    public UpdateCardCommandHandler(ICurrentUserProvider currentUserProvider, 
+        CardAppDbContext dbContext, 
+        CardEntityToResponseMapper mapper)
+    {
+        _currentUserProvider = currentUserProvider;
+        _dbContext = dbContext;
+        _mapper = mapper;
+    }
+
+    public async Task<ApiResult<CardResponse?>> Handle(UpdateCardCommand request, CancellationToken cancellationToken)
+    {
+        var card = await _dbContext.Cards.FirstOrDefaultAsync(c =>
+            c.Id == request.Id && c.UserId == _currentUserProvider.UserId, cancellationToken: cancellationToken);
+        if (card == null) return ResponseMessage<CardResponse?>.Error(null, "Card not found", ResponseCodes.NotFound);
+
+        if (!string.IsNullOrEmpty(request.Name)) card.Name = request.Name; //important check to ensure the name is not empty
+
+        card.Color = request.Color;
+        card.Description = request.Description;
+        card.Status = request.Status;
+        
+        var affectedRows = await _dbContext.SaveChangesAsync(cancellationToken);
+        
+        if (affectedRows > 0)
+        {
+            return ResponseMessage<CardResponse?>.Success(_mapper.MapToResponse(card), 
+                "Card successfully updated", ResponseCodes.Created);
+        }
+        
+        return ResponseMessage<CardResponse?>.Error(null, "Unable to update card");
+    }
+}
