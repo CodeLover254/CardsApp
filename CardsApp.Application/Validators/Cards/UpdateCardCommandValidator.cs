bb89efd55e08613547b3e5deb41cdb92e6b67e19
Patrick <pmwangi@bridgeinnovation.io>
2024-02-13 20:53:27 +0300

    chore: Add validator for updates

---
+++ CardsApp.Application/Validators/Cards/UpdateCardCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using CardsApp.Application.Commands.Cards;
+using FluentValidation;
+
+namespace CardsApp.Application.Validators.Cards;
+
+public class UpdateCardCommandValidator: BaseCardValidator<UpdateCardCommand>
+{
+    public UpdateCardCommandValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty();
+        RuleFor(x => x.Color).Must(BeValidColor!)
+            .When(x => !string.IsNullOrEmpty(x.Color))
+            .WithMessage("Invalid hex color code");
+        RuleFor(x => x.Status).IsInEnum();
+    }
+}
