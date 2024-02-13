a45c15148131905044e4042a7293ff7932bc2099
Patrick <pmwangi@bridgeinnovation.io>
2024-02-13 20:46:29 +0300

    chore: refactor validator to use base

--- CardsApp.Application/Validators/Cards/CreateCardCommandValidator.cs
+++ CardsApp.Application/Validators/Cards/CreateCardCommandValidator.cs
@@ -1,12 +1,18 @@
-﻿using CardsApp.Application.Commands.Cards;
+﻿using System.Text.RegularExpressions;
+using CardsApp.Application.Commands.Cards;
 using FluentValidation;
 
 namespace CardsApp.Application.Validators.Cards;
 
-public class CreateCardCommandValidator: AbstractValidator<CreateCardCommand>
+public class CreateCardCommandValidator: BaseCardValidator<CreateCardCommand>
 {
     public CreateCardCommandValidator()
     {
-        //todo complete validation
+        RuleFor(x => x.Name).NotEmpty();
+        RuleFor(x => x.Color).Must(BeValidColor!)
+            .When(x => !string.IsNullOrEmpty(x.Color))
+            .WithMessage("Invalid hex color code");
     }
+
+    
 }
