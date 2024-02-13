c459a639705b4f2bef9f90619c4a02cbec49097c
Patrick <pmwangi@bridgeinnovation.io>
2024-02-13 22:24:44 +0300

    chore: Add fields for sorting and filtering

--- CardsApp.Domain/Dto/Cards/BasePaginatedItemsQuery.cs
+++ CardsApp.Domain/Dto/Cards/BasePaginatedItemsQuery.cs
@@ -1,7 +1,12 @@
-﻿namespace CardsApp.Domain.Dto.Cards;
+﻿using CardsApp.Domain.Enums;
+
+namespace CardsApp.Domain.Dto.Cards;
 
 public class BasePaginatedItemsQuery
 {
     public int Index { get; set; } = 0;
     public int PageSize { get; set; } = 10;
+    public CardFilterables? FilterBy { get; set; }
+    public string? SearchTerm { get; set; }
+    public CardFilterables? SortBy { get; set; }
 }
