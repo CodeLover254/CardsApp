9bbddec9f4fa8df318c1f13dd06a92c6e1ca8bb3
Patrick <pmwangi@bridgeinnovation.io>
2024-02-13 23:16:47 +0300

    chore: Create BaseController

---
+++ CardsApp.Api/Controllers/BaseController.cs
@@ -0,0 +1,21 @@
+﻿using CardsApp.Domain.Dto.Results;
+using CardsApp.Domain.Enums;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CardsApp.Api.Controllers;
+
+[Authorize]
+public class BaseController : ControllerBase
+{
+    public IActionResult CustomResponse<T>(ApiResult<T> apiResult)
+    {
+        return apiResult.ResponseCode switch
+        {
+            ResponseCodes.Created => Created(string.Empty, apiResult),
+            ResponseCodes.Ok => Ok(apiResult),
+            ResponseCodes.Failed => BadRequest(apiResult),
+            ResponseCodes.NotFound => NotFound(apiResult)
+        };
+    }
+}
