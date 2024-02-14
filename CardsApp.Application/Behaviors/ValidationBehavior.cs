2e67ebb27222fb06a5b80ad097a2a624d0384442
Patrick <astroguypasa@gmail.com>
2024-02-14 12:26:49 +0300

    chore: Add ValidationBehavior logic

--- CardsApp.Application/Behaviors/ValidationBehavior.cs
+++ CardsApp.Application/Behaviors/ValidationBehavior.cs
@@ -1,12 +1,30 @@
-﻿using MediatR;
+﻿using FluentValidation;
+using MediatR;
 
 namespace CardsApp.Application.Behaviors;
 
-public class ValidationBehavior<TRequest,TResponse>: IPipelineBehavior<TRequest, TResponse>
+public class ValidationBehavior<TRequest,TResponse>: IPipelineBehavior<TRequest, TResponse> where TRequest: class, IRequest<TResponse>
 {
-    public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
     {
-        //todo complete
-        throw new NotImplementedException();
+        _validators = validators;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (!_validators.Any()) return await next();
+
+        var validationCtx = new ValidationContext<TRequest>(request);
+
+        var errors = _validators.Select(v => v.Validate(validationCtx))
+            .SelectMany(x => x.Errors)
+            .Where(f => f != null)
+            .ToList();
+
+        if (errors.Any()) throw new ValidationException("Validation failure", errors);
+
+        return await next();
     }
 }
