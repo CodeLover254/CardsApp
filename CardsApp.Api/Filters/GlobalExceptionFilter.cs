88e1ec0e2a7b9c4b0d359831fbdf5c206751c8f7
Patrick <astroguypasa@gmail.com>
2024-02-14 12:25:06 +0300

    chore: Add GlobalExceptionFilter

--- CardsApp.Api/Filters/GlobalExceptionFilter.cs
+++ CardsApp.Api/Filters/GlobalExceptionFilter.cs
@@ -1,6 +1,44 @@
-﻿namespace CardsApp.Api.Filters;
+﻿using CardsApp.Domain.Dto.Results;
+using CardsApp.Domain.Enums;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
-public class GlobalExceptionFilter
+namespace CardsApp.Api.Filters;
+
+public class GlobalExceptionFilter: IExceptionFilter
 {
-    //todo complete
+    private readonly ILogger<GlobalExceptionFilter> _logger;
+
+    public GlobalExceptionFilter(ILogger<GlobalExceptionFilter> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnException(ExceptionContext context)
+    {
+        var traceId = Guid.NewGuid().ToString();
+        ApiResult<string> apiResult = new ApiResult<string>
+        {
+            Result = null,
+            ResponseCode = ResponseCodes.Failed
+        };
+
+        if (context.Exception.GetType() == typeof(ValidationException))
+        {
+            var validationException = context.Exception as ValidationException;
+            apiResult.Errors = validationException!.Errors.Select(x => x.ErrorMessage).ToList();
+            apiResult.Message = "Invalid inputs.";
+            context.Result = new BadRequestObjectResult(apiResult);
+        }
+        else
+        {
+            _logger.LogError(context.Exception,"An exception occurred during a request with message {message}. Trace Id: {traceId}",context.Exception.Message,traceId);
+            apiResult.Message = $"Something went wrong. Please try again later. Trace Id: {traceId}";
+            context.Result = new BadRequestObjectResult(apiResult);
+            context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        }
+
+        context.ExceptionHandled = true;
+    }
 }
