14a4c8a3c0cde80fb6d3ec76d9ff69c8051bebab
Patrick <astroguypasa@gmail.com>
2024-02-15 10:48:41 +0300

    chore: Add BaseCardsQueryableBuilder

---
+++ CardsApp.Application/Services/BaseCardsQueryableBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq.Expressions;
+using CardsApp.Application.Interfaces;
+using CardsApp.Domain;
+using CardsApp.Domain.Constants;
+using CardsApp.Domain.Entities;
+
+namespace CardsApp.Application.Services;
+
+public class BaseCardsQueryableBuilder
+{
+    protected readonly CardAppDbContext DbContext;
+    protected readonly ICurrentUserProvider CurrentUserProvider;
+    
+    public BaseCardsQueryableBuilder(CardAppDbContext dbContext, ICurrentUserProvider currentUserProvider)
+    {
+        DbContext = dbContext;
+        CurrentUserProvider = currentUserProvider;
+    }
+
+    public IQueryable<Card> BuildQuery(Expression<Func<Card,bool>>? query=null)
+    {
+        var cardQueryable = DbContext.Cards.AsQueryable();
+        if (CurrentUserProvider.UserRole!.Equals(UserRoles.Member))
+        {
+            cardQueryable = cardQueryable.Where(x => x.UserId == CurrentUserProvider.UserId);
+        }
+
+        if (query == null) return cardQueryable;
+        
+        return cardQueryable.Where(query);
+    }
+}
