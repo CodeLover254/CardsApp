7b8c01f95444f3c57b14dff06e3718eb57e739dc
Patrick <astroguypasa@gmail.com>
2024-02-15 11:07:37 +0300

    chore: replace enum with class based constants

---
+++ CardsApp.Domain/Constants/CardStatus.cs
@@ -0,0 +1,8 @@
+namespace CardsApp.Domain.Constants;
+
+public class CardStatus
+{
+    public const string ToDo = "To Do";
+    public const string InProgress = "In Progress";
+    public const string Done = "Done";
+}
--- CardsApp.Domain/Enums/CardStatus.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CardsApp.Domain.Enums;
-
-public enum CardStatus
-{
-    ToDo=1,
-    InProgress=2,
-    Done=3
-}
